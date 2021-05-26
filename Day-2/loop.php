17a6fadd45ebbfc660502380e0ff317fecdb08c6
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-05-26 14:00:39 +0530

    Add files via upload

---
+++ Day-2/loop.php
@@ -0,0 +1,12 @@
+
+      <?php
+         $a = 0;
+         $b = 0;
+         
+         for( $i = 0; $i<5; $i++ ) {
+            $a += 20;
+            $b += 10;
+         }
+         
+         echo ("At the end of the loop a = $a and b = $b" );
+      ?>
