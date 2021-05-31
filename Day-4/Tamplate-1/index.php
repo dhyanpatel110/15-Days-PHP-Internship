208da3e9c51c08cf470e25f200cfc0c152862a2f
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-05-31 12:39:22 +0530

    Add files via upload

---
+++ Day-4/Tamplate-1/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>PlusBusiness</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="layout/styles/layout.css" type="text/css" />
+<script type="text/javascript" src="layout/scripts/jquery.min.js"></script>
+<script type="text/javascript" src="layout/scripts/jquery.jcarousel.pack.js"></script>
+<script type="text/javascript" src="layout/scripts/jquery.jcarousel.setup.js"></script>
+</head>
+<body id="top">
+  
+<?php
+include './pages/header.php';
+?>
+
+<?php
+include './pages/slider.php';
+?>
+
+<?php
+include './pages/body.php';
+?>
+
+<?php
+include './pages/footer.php';
+?>
+
+</body>
+</html>
