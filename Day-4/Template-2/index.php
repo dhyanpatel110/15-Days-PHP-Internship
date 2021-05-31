d8832b001117a27f71d99bf03e117a4d96b0fb5f
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-05-31 14:09:03 +0530

    Add files via upload

---
+++ Day-4/Template-2/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Modular Business</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="layout/styles/layout.css" type="text/css" />
+<script type="text/javascript" src="layout/scripts/jquery.min.js"></script>
+<script type="text/javascript" src="layout/scripts/jquery.jcarousel.pack.js"></script>
+<script type="text/javascript" src="layout/scripts/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="layout/scripts/jquery.jcarousel.setup.js"></script>
+</head>
+<body id="top">
+<div class="wrapper col1">
+  <div id="topbar">
+    <p>Tel: xxxxx xxxxxxxxxx | Mail: info@domain.com</p>
+    <ul>
+      <li><a href="#">Libero</a></li>
+      <li><a href="#">Maecenas</a></li>
+      <li><a href="#">Mauris</a></li>
+      <li class="last"><a href="#">Suspendisse</a></li>
+    </ul>
+    <br class="clear" />
+  </div>
+</div>
+<!-- ####################################################################################################### -->
+<div class="wrapper col2">
+  <div id="header">
+    <div class="fl_left">
+      <h1><a href="index.html">Modular Business</a></h1>
+      <p>Free Website Template</p>
+    </div>
+    <div class="fl_right"> <a href="#"><img src="images/demo/468x60.gif" alt="" /></a> </div>
+    <br class="clear" />
+  </div>
+</div>
+
+<?php
+include './pages/navigation.php';
+?>
+
+<?php
+include './pages/slider.php';
+?>
+
+<?php
+include './pages/body.php';
+?>
+
+<?php
+include './pages/footer.php';
+?>
+
+</body>
+</html>
