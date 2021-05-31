ed410b33ce8cdd06f849278aac4ff348da5c1377
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-05-31 14:06:59 +0530

    Add files via upload

---
+++ Day-4/Template-2/pages/body.php
@@ -0,0 +1,33 @@
+<!-- ########################################---start body---################################################# -->
+<div class="wrapper col5">
+  <div id="container">
+    <div id="content">
+      <h2>About This Free CSS Template</h2>
+      <img class="imgl" src="images/demo/imgl.gif" alt="" width="125" height="125" />
+      <p><strong>Indonectetus facilis leo nibh</strong></p>
+      <p>This is a W3C standards compliant free website template from <a href="http://www.os-templates.com/">OS Templates</a>.</p>
+      <p>This template is distributed using a <a href="http://www.os-templates.com/template-terms">Website Template Licence</a>, which allows you to use and modify the template for both personal and commercial use when you keep the provided credit links in the footer.</p>
+      <p>For more CSS templates visit <a href="http://www.os-templates.com/">Free Website Templates</a>.</p>
+      <p>Lacusenim inte trices lorem anterdum nam sente vivamus quis fauctor mauris wisinon vivamus. Condimentumfelis et amet tellent quisquet a leo lacus nec augue accumsan. Sagittislaorem dolor ipsum at urna et pharetium malesuada nis consectus odio.</p>
+    </div>
+    <div id="column">
+      <div class="holder">
+        <h2>Nullamlacus loborttis</h2>
+        <ul id="latestnews">
+          <li><img class="imgl" src="images/demo/80x80.gif" alt="" />
+            <p><strong>Indonectetus facilis leo nibh.</strong></p>
+            <p>Nullamlacus dui ipsum cons eque loborttis non euis que morbi penas dapibulum orna.</p>
+            <p class="readmore"><a href="#">Continue Reading &raquo;</a></p>
+          </li>
+          <li class="last"><img class="imgl" src="images/demo/80x80.gif" alt="" />
+            <p><strong>Indonectetus facilis leo nibh.</strong></p>
+            <p>Nullamlacus dui ipsum cons eque loborttis non euis que morbi penas dapibulum orna.</p>
+            <p class="readmore"><a href="#">Continue Reading &raquo;</a></p>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <br class="clear" />
+  </div>
+</div>
+<!-- ########################################---end body---################################################# -->
---
+++ Day-4/Template-2/pages/navigation.php
@@ -0,0 +1,20 @@
+<!-- #############################################---start navigation---#################################### -->
+<div class="wrapper col3">
+  <div id="topnav">
+    <ul>
+      <li class="active"><a href="index.html">Homepage</a><span>Test Text Here</span></li>
+      <li><a href="#">Style Demo</a><span>Test Text Here</span></li>
+      <li><a href="#">Full Width</a><span>Test Text Here</span></li>
+      <li><a href="#">DropDown</a><span>Test Text Here</span>
+        <ul>
+          <li><a href="#">Link 1</a></li>
+          <li><a href="#">Link 2</a></li>
+          <li><a href="#">Link 3</a></li>
+        </ul>
+      </li>
+      <li class="last"><a href="#">Link text</a><span>Test Text Here</span></li>
+    </ul>
+    <br class="clear" />
+  </div>
+</div>
+<!-- #############################################---end navigation---#################################### -->
