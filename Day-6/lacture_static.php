239e2158850afddc2ca4f73937dcc7048d6b3934
DHYAN PATEL <62758631+dhyanpatel110@users.noreply.github.com>
2021-06-01 11:22:50 +0530

    Add files via upload

---
+++ Day-6/lacture_static.php
@@ -0,0 +1,31 @@
+<?php
+
+$host="localhost";
+$username="root";
+$passwd="";
+$dbname="db_internship";
+
+//connection function
+$connection = mysqli_connect($host , $username ,$passwd ,$dbname);
+
+$q = mysqli_query($connection,
+            "INSERT INTO tbl_user(user_name,user_gender,user_mobile) VALUES ('nop','male','21261827')")
+                  or die("Error".mysqli_error($connection));
+
+if($q){
+    echo"<script>alert('Record Added');</script>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
